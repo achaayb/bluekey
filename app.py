1e67700c2aa381069436d0f8797d2cc25052245f
621944283 <youssefchaabi949@gmail.com>
2023-02-28 03:51:28 +0100

    Done

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from collections import defaultdict
 from datetime import datetime
 from sqlalchemy import Integer, Enum
+from flask_migrate import Migrate
 import enum
 import random
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
 db = SQLAlchemy(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 def get_random_string(length):
     # choose from all lowercase letter
     letters = string.ascii_lowercase
@@ -19,10 +22,11 @@ def get_random_string(length):
 
 class Crypters(db.Model):
     __tablename__ = 'crypters'
-    id = db.Column(db.String, primary_key=True, default=get_random_string(6))
+    id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(200))
     product_image = db.Column(db.String(50000))
     product_description = db.Column(db.String(10000)) 
+    price = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -30,10 +34,11 @@ class Crypters(db.Model):
 
 class Remotes(db.Model):
     __tablename__ = 'remotes'
-    id = db.Column(db.String, primary_key=True, default=get_random_string(6))
+    id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(200))
     product_image = db.Column(db.String(50000))
     product_description = db.Column(db.String(10000)) 
+    price = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -41,50 +46,54 @@ class Remotes(db.Model):
 
 class Exploits(db.Model):
     __tablename__ = 'exploits'
-    id = db.Column(db.String, primary_key=True, default=get_random_string(6))
+    id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(200))
     product_image = db.Column(db.String(50000))
     product_description = db.Column(db.String(10000)) 
+    price = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return self.id
 
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/backoffice/', methods=["POST", "GET"])
 def index():
     return render_template("index.html")
 
 #################### CRYPTERS
 
-@app.route('/crypters/', methods=["POST", "GET"])
+@app.route('/backoffice/crypters/', methods=["POST", "GET"])
 def viewCrypter():
     if (request.method == "POST") and ('product_name' in request.form):
         product_name = request.form["product_name"]
         product_image = request.form["product_image"]
         product_description = request.form["product_description"]
-        new_product = Crypters(product_name=product_name, product_image=product_image, product_description=product_description)
+        price = request.form["price"]
+        price = int(price)
+        new_product = Crypters(product_name=product_name, product_image=product_image, product_description=product_description,price=price)
 
         try:
             db.session.add(new_product)
             db.session.commit()
-            return redirect("/crypters/")
+            return redirect("/backoffice/crypters/")
 
         except:
+          
             crypters = Crypters.query.order_by(Crypters.date_created).all()
             return "There Was an issue while add a new Product"
     else:
         crypters = Crypters.query.order_by(Crypters.date_created).all()
         return render_template("crypters.html", crypters=crypters)
 
-@app.route("/delete-crypter/<name>")
+@app.route("/backoffice/delete-crypter/<name>")
 def deleteProduct(name):
     product_to_delete = Crypters.query.get_or_404(name)
 
     try:
         db.session.delete(product_to_delete)
         db.session.commit()
-        return redirect("/crypters/")
+        return redirect("/backoffice/crypters/")
     except:
         return "There was an issue while deleteing the Product"
 
@@ -92,18 +101,19 @@ def deleteProduct(name):
 
 #################### EXPLOITS
 
-@app.route('/exploits/', methods=["POST", "GET"])
+@app.route('/backoffice/exploits/', methods=["POST", "GET"])
 def viewExploit():
     if (request.method == "POST") and ('product_name' in request.form):
         product_name = request.form["product_name"]
         product_image = request.form["product_image"]
         product_description = request.form["product_description"]
-        new_product = Exploits(product_name=product_name, product_image=product_image, product_description=product_description)
+        price = request.form["price"]
+        new_product = Exploits(product_name=product_name, product_image=product_image, product_description=product_description,price=price)
 
         try:
             db.session.add(new_product)
             db.session.commit()
-            return redirect("/exploits/")
+            return redirect("/backoffice/exploits/")
 
         except:
             exploits = Exploits.query.order_by(Exploits.date_created).all()
@@ -112,14 +122,14 @@ def viewExploit():
         exploits = Exploits.query.order_by(Exploits.date_created).all()
         return render_template("exploits.html", exploits=exploits)
 
-@app.route("/delete-exploit/<name>")
+@app.route("/backoffice/delete-exploit/<name>")
 def deleteExploit(name):
     product_to_delete = Exploits.query.get_or_404(name)
 
     try:
         db.session.delete(product_to_delete)
         db.session.commit()
-        return redirect("/exploits/")
+        return redirect("/backoffice/exploits/")
     except:
         return "There was an issue while deleteing the Product"
 
@@ -127,18 +137,19 @@ def deleteExploit(name):
 
 #################### REMOTES
 
-@app.route('/remotes/', methods=["POST", "GET"])
+@app.route('/backoffice/remotes/', methods=["POST", "GET"])
 def viewRemote():
     if (request.method == "POST") and ('product_name' in request.form):
         product_name = request.form["product_name"]
         product_image = request.form["product_image"]
         product_description = request.form["product_description"]
-        new_product = Remotes(product_name=product_name, product_image=product_image, product_description=product_description)
+        price = request.form["price"]
+        new_product = Remotes(product_name=product_name, product_image=product_image, product_description=product_description,price=price)
 
         try:
             db.session.add(new_product)
             db.session.commit()
-            return redirect("/remotes/")
+            return redirect("/backoffice/remotes/")
 
         except:
             remotes = Remotes.query.order_by(Remotes.date_created).all()
@@ -147,18 +158,41 @@ def viewRemote():
         remotes = Remotes.query.order_by(Remotes.date_created).all()
         return render_template("remotes.html", remotes=remotes)
 
-@app.route("/delete-remote/<name>")
+@app.route("/backoffice/delete-remote/<name>")
 def deleteRemote(name):
     product_to_delete = Remotes.query.get_or_404(name)
 
     try:
         db.session.delete(product_to_delete)
         db.session.commit()
-        return redirect("/remotes/")
+        return redirect("/backoffice/remotes/")
     except:
         return "There was an issue while deleteing the Product"
 
-#################### CRYPTERS
+#################### Order
+
+@app.route('/order/')
+@app.route('/order/home')
+def home():
+    return render_template('Order/index.html')
+
+@app.route('/order/crypters')
+def crypters():
+    crypters = Crypters.query.order_by(Crypters.date_created).all()
+    return render_template('Order/index.php?rp=%2Fstore%2Fcrypters.html',crypters=crypters)
+
+@app.route('/order/silent office')
+def silent():
+    exploits = Exploits.query.order_by(Exploits.date_created).all()
+    return render_template('Order/cart.php?a=confproduct&i=15&language=ukranian.html',exploits=exploits)
+
+
+@app.route('/order/remote administrator')
+def remote():
+    
+    remotes = Remotes.query.order_by(Remotes.date_created).all()
+    return render_template('Order/index.php?rp=%2Fstore%2Fremote-administrator-tool.html',remotes=remotes)
+
 
 
 if (__name__ == "__main__"):
